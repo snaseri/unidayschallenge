da50ebdef041d30fa6952cce3aef7c7af1e83cbc
Sepehr Naseri <sepehr2000n@icloud.com>
2020-02-20 03:40:49 +0000

    new function to calculate discount

--- src/com/unidays/challenge/Main.java
+++ src/com/unidays/challenge/Main.java
@@ -4,15 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
+        ShopItem A = new ShopItem("A", 8.0); //No discount
+        ShopItem B = new ShopItem("B", 12.0); //2 for 20£
+        ShopItem C = new ShopItem("C", 4.0); //3 for 10£
+        ShopItem D = new ShopItem("D", 7.0); //Buy one get one free
+        ShopItem E = new ShopItem("E", 5.0); //3 for the price of 2
+
         ShopItem item = new ShopItem("Pizza", 10.50);
         UnidaysDiscountChallenge unidaysDiscountChallenge = new UnidaysDiscountChallenge();
 
-        unidaysDiscountChallenge.AddToBasket(item);
-        unidaysDiscountChallenge.AddToBasket(item);
-        unidaysDiscountChallenge.AddToBasket(item);
-        unidaysDiscountChallenge.AddToBasket(item);
-
-        System.out.println(unidaysDiscountChallenge.CalculateTotalPrice());
+        System.out.println("Total price is " + unidaysDiscountChallenge.CalculateTotalPrice());
 	// write your code here
     }
 }
--- src/com/unidays/challenge/UnidaysDiscountChallenge.java
+++ src/com/unidays/challenge/UnidaysDiscountChallenge.java
@@ -14,14 +14,80 @@ public class UnidaysDiscountChallenge {
     }
 
     public double CalculateTotalPrice() {
+
+        double discount = this.getDiscount();
+
         double totalPrice = 0.00;
         for (ShopItem item: basket) {
             totalPrice = item.getPrice() + totalPrice;
         }
+        totalPrice = totalPrice - discount;
+
         if (totalPrice >= freeDeliveryPrice){
-            return totalPrice;
+            System.out.println("This order is over 50£ and hence it is eligible for free delivery");
+            System.out.println("Delivery Charge = £" + 0.00);
+            return totalPrice - discount;
         } else {
-            return totalPrice+deliveryCharge;
+            System.out.println("This order is under 50£ and hence not eligible for free delivery");
+            System.out.println("Delivery Charge = £" + deliveryCharge);
+            return (totalPrice + deliveryCharge);
         }
+    }
+
+    public double getDiscount() {
+        double discountAmount = 0.0;
+        int b = 0;
+        int c = 0;
+        int d = 0;
+        int e = 0;
+
+        for (ShopItem item: basket) {
+            switch(item.getName()) {
+                case "B":
+                    b = b +1;
+                    break;
+                case "C":
+                    c = c +1;
+                    break;
+                case "D":
+                    d = d +1;
+                    break;
+                case "E":
+                    e = e + 1;
+                    break;
+            }
+        }
+        //only dividing by 2 as the division only returns the integer part of the value
+        //for example if only 1 item = b / 2 = 0
+        discountAmount = discountAmount + ((b / 2) * 4);
+
+
+        //3 for 10£
+        if (c%3 == 1) {
+            c--;
+            discountAmount = discountAmount + ((c/3) * 2);
+        } else if (c%3 == 2) {
+            c = c-2;
+            discountAmount = discountAmount +  ((c/3) * 2);
+        } else {
+            discountAmount = discountAmount +  ((c/3) * 2);
+        }
+
+        //Buy one get one free
+        discountAmount = discountAmount + ((d/2) * 7);
+        //3 for the price of 2
+        if (e%3 == 1) {
+            e--;
+            discountAmount = discountAmount + ((e/3) * 5);
+        } else if (e%3 == 2) {
+            e = e-2;
+            discountAmount = discountAmount +  ((e/3) * 5);
+        } else {
+            discountAmount = discountAmount +  ((e/3) * 5);
+        }
+
+        System.out.println(discountAmount);
+// 1 2
+        return discountAmount;
     }
 }
