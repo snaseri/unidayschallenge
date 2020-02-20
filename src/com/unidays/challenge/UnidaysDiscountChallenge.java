6db7be42b71936869ed1328248a71cc48545ca1b
Sepehr Naseri <sepehr2000n@icloud.com>
2020-02-20 02:32:13 +0000

    Orders over 50£ now have free delivery

--- src/com/unidays/challenge/UnidaysDiscountChallenge.java
+++ src/com/unidays/challenge/UnidaysDiscountChallenge.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 public class UnidaysDiscountChallenge {
 
     private ArrayList<ShopItem> basket = new ArrayList<>();
+    private final double deliveryCharge = 7.00;
+    private final double freeDeliveryPrice= 50.00;
 
     public void AddToBasket(ShopItem item) {
         basket.add(item);
@@ -16,6 +18,10 @@ public class UnidaysDiscountChallenge {
         for (ShopItem item: basket) {
             totalPrice = item.getPrice() + totalPrice;
         }
-        return totalPrice;
+        if (totalPrice >= freeDeliveryPrice){
+            return totalPrice;
+        } else {
+            return totalPrice+deliveryCharge;
+        }
     }
 }
